412d2aa07e6fb5347e629a9bcc3b159a7b58cbd5
Vasil Kosturski <vasilkosturski@gmail.com>
2022-03-19 10:25:31 +0200

    Added set_env.sh

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=vasilkosturski
+export POSTGRES_PASSWORD=fake_pass
+export POSTGRES_HOST=udagram.c0dds5jp4tiy.eu-central-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-259984666282
+export AWS_REGION=eu-central-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
+
